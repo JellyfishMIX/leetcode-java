9dbf8f3c40fa108e3a09d04e291cd5e9b1d92867
qianshijie <2273516339@qq.com>
2021-08-05 14:30:13 +0800

    Add a solution of problem no.206

---
+++ src/no206/ListNode.java
@@ -0,0 +1,22 @@
+package no206;
+
+/**
+ * @author JellyfishMIX
+ * @date 2021/8/5 13:49
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
