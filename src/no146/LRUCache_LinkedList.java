b2b1d77221bab23ba08243e5a5bd69e9faf35226
qianshijie <2273516339@qq.com>
2021-08-11 14:40:51 +0800

    Add a solution of problem 146. LRU 缓存机制 - LinkedList 解法

---
+++ src/no146/LRUCache_LinkedList.java
@@ -0,0 +1,71 @@
+package no146;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 146. LRU 缓存机制
+ * LinkedList 解法
+ * link: https://leetcode-cn.com/problems/lru-cache/
+ *
+ * @author JellyfishMIX
+ * @date 2021/8/10 19:38
+ */
+public class LRUCache_LinkedList {
+    private class Element {
+        int key;
+        int value;
+        long invokeTime;
+
+        public Element(int key, int value, long invokeTime) {
+            this.key = key;
+            this.value = value;
+            this.invokeTime = invokeTime;
+        }
+    }
+
+    private List<Element> list;
+    // 容量
+    private int capacity;
+
+    public LRUCache_LinkedList(int capacity) {
+        list = new LinkedList<>();
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        for (Element element : list) {
+            if (element.key == key) {
+                // 更新调用时间
+                element.invokeTime = System.currentTimeMillis();
+                return element.value;
+            }
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        // 调用时间
+        long now = System.currentTimeMillis();
+        if (list.size() < capacity) {
+            list.add(new Element(key, value, now));
+            // 寻找最久未使用的元素，置换出去
+        } else {
+            long earliestInvokeTime = Long.MAX_VALUE;
+            int earliestIndex = -1;
+            int index = 0;
+            for (Element element : list) {
+                if (element.invokeTime < earliestInvokeTime) {
+                    earliestInvokeTime = element.invokeTime;
+                    earliestIndex = index;
+                }
+                // 索引++;
+                index++;
+            }
+            // 置换掉最久未使用的元素
+            list.remove(earliestIndex);
+            // 放入新的元素
+            list.add(new Element(key, value, now));
+        }
+    }
+}
