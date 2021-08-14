80dd7c9a13ca66b85aebd488720498180b380811
qianshijie <2273516339@qq.com>
2021-08-14 10:47:54 +0800

    Add a solution of problem 160. 相交链表

---
+++ src/no160/Solution.java
@@ -0,0 +1,20 @@
+package no160;
+
+/**
+ * 160. 相交链表
+ * link: https://leetcode-cn.com/problems/intersection-of-two-linked-lists/
+ *
+ * @author JellyfishMIX
+ * @date 2021/8/14 10:35
+ */
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
