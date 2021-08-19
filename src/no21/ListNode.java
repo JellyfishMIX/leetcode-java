c2c4499d8f5ff1866810c0deaff273465ae4c228
qianshijie <2273516339@qq.com>
2021-08-19 20:44:32 +0800

    Add a solution of problem 21. 合并两个有序链表

---
+++ src/no21/ListNode.java
@@ -0,0 +1,22 @@
+package no21;
+
+/**
+ * @author JellyfishMIX
+ * @date 2021/8/19 19:37
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
