c55c061eb526a2e808c8054edcba46ba137aec86
qianshijie <2273516339@qq.com>
2021-08-21 22:55:12 +0800

    Add a solution of problem 112. 路径总和

--- src/no112/Solution.java
+++ src/no112/Solution.java
@@ -15,6 +15,6 @@ public class Solution {
         if (root.left == null && root.right == null) {
             return targetSum == root.val;
         }
-        return hasPathSum(root, targetSum - root.val) || hasPathSum(root, targetSum - root.val);
+        return hasPathSum(root.left, targetSum - root.val) || hasPathSum(root.right, targetSum - root.val);
     }
 }
