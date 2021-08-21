e6c9110dc43dd49d07dd90d1042b7e3afdc0b9be
qianshijie <2273516339@qq.com>
2021-08-21 22:53:10 +0800

    Add a solution of problem 112. 路径总和

---
+++ src/no112/TreeNode.java
@@ -0,0 +1,18 @@
+package no112;
+
+/**
+ * @author JellyfishMIX
+ * @date 2021/8/21 11:28
+ */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
