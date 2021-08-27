b2bd7d3d7d6d8538551b4d5362880677ccc99a77
qianshijie <2273516339@qq.com>
2021-08-27 23:09:59 +0800

    Add a solution of problem 104. 二叉树的最大深度

---
+++ src/first/no104/Solution.java
@@ -0,0 +1,19 @@
+package first.no104;
+
+/**
+ * 104. 二叉树的最大深度
+ * link: https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
+ *
+ * @author JellyfishMIX
+ * @date 2021/8/27 22:26
+ */
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
