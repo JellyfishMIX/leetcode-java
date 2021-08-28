3ef9091f0b374af193e7ad3415017a2571277c06
qianshijie <2273516339@qq.com>
2021-08-29 04:29:01 +0800

    Add a solution of problem 450. 删除二叉搜索树中的节点

---
+++ src/first/no450/TreeNode.java
@@ -0,0 +1,24 @@
+package first.no450;
+
+/**
+ * @author JellyfishMIX
+ * @date 2021/8/29 03:38
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
