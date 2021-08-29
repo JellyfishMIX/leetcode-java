7c3d7df0ee0fd3ba732565d8e0482a7146f34f8b
qianshijie <2273516339@qq.com>
2021-08-29 22:52:09 +0800

    Add a solution of problem 234. 回文链表

---
+++ src/first/no234/ListNode.java
@@ -0,0 +1,22 @@
+package first.no234;
+
+/**
+ * @author JellyfishMIX
+ * @date 2021/8/29 22:45
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
