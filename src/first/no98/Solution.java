e77d9be4b4cb813281de44f16b97237a8591bf47
qianshijie <2273516339@qq.com>
2021-08-30 12:56:22 +0800

    Add a solution of problem 98. 验证二叉搜索树

---
+++ src/first/no98/Solution.java
@@ -0,0 +1,28 @@
+package first.no98;
+
+/**
+ * 98. 验证二叉搜索树
+ * link: https://leetcode-cn.com/problems/validate-binary-search-tree/submissions/
+ *
+ * @author JellyfishMIX
+ * @date 2021/8/30 00:35
+ */
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, null, null);
+    }
+
+    private boolean isValidBST(TreeNode root, TreeNode min, TreeNode max) {
+        // root
+        if (root == null) {
+            return true;
+        }
+        if (min != null && root.val <= min.val) {
+            return false;
+        }
+        if (max != null && root.val >= max.val) {
+            return false;
+        }
+        return isValidBST(root.left, min, root) && isValidBST(root.right, root, max);
+    }
+}
---
+++ src/first/no98/TreeNode.java
@@ -0,0 +1,24 @@
+package first.no98;
+
+/**
+ * @author JellyfishMIX
+ * @date 2021/8/30 00:35
+ */
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
