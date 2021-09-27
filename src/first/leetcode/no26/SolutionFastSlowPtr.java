7a470a2fe05b291bd8b70f54861e1e7fd8bff268
qianshijie <2273516339@qq.com>
2021-09-27 19:27:41 +0800

    Add a solution of problem 26. 删除有序数组中的重复项

--- src/first/leetcode/no26/SolutionFastSlowPtr.java
+++ src/first/leetcode/no26/SolutionFastSlowPtr.java
@@ -12,12 +12,11 @@ public class SolutionFastSlowPtr {
     public int removeDuplicates(int[] nums) {
         int slow = 0, fast = 1;
         while (fast < nums.length) {
-            if (nums[slow] == nums[fast]) {
-                fast++;
-            } else {
+            if (nums[slow] != nums[fast]) {
                 nums[slow + 1] = nums[fast];
                 slow++;
             }
+            fast++;
         }
         return slow + 1;
     }
