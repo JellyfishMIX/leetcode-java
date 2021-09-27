47984e10c1e9cb731897f6a0fdccc38a13fc3cd0
qianshijie <2273516339@qq.com>
2021-09-27 19:24:22 +0800

    Add a solution of problem 26. 删除有序数组中的重复项

--- src/first/leetcode/no26/SolutionFastSlowPtr.java
+++ src/first/leetcode/no26/SolutionFastSlowPtr.java
@@ -3,7 +3,7 @@ package first.leetcode.no26;
 /**
  * 26. 删除有序数组中的重复项
  * link: https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
- * 题解：老婆牛牛
+ * 题解：快慢指针 -- 老婆牛牛亲自讲解
  *
  * @author JellyfishMIX
  * @date 2021/9/27 18:52
